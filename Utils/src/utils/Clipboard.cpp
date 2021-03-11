8baba0ed5d7cd77a609deac59721dd867c853781
vikonix <valeriy.kovalev.software@gmail.com>
2021-03-12 00:12:25 +0100

    fix build

--- Utils/src/utils/Clipboard.cpp
+++ Utils/src/utils/Clipboard.cpp
@@ -25,7 +25,7 @@ ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
 SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
 #include "utils/Clipboard.h"
-#include "utils/Logger.h"
+#include "utils/logger.h"
 #include "utfcpp/utf8.h"
 
 #ifdef WIN32
